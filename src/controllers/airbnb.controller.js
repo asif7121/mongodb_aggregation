dc713a2fda0c49053e48dc0ff7b664400600520d
Asif <ansariasif449@gmail.com>
2024-06-20 16:54:34 +0530

    fixed status code response
    Signed-off-by: Asif <ansariasif449@gmail.com>

--- src/controllers/airbnb.controller.js
+++ src/controllers/airbnb.controller.js
@@ -22,7 +22,7 @@ export const getPropertyType = async (req, res) => {
     sortObj:{_id:1}
   }
   const response = await databaseHelper.GenerateQueryWithPagination(options, Airbnb)
-  return res.json({ ...response })
+  return res.status(200).json({ ...response })
   } catch (error) {
 		console.log('Error : ', error)
 		res.status(500).json({ error: error.message })
@@ -30,90 +30,94 @@ export const getPropertyType = async (req, res) => {
 }
 
 export const searchProperty = async (req, res) => {
-	const {
+	try {
+    const {
 		property_type,
 		accommodates,
 		startIndex = 1,
 		itemsPerPage = 10,
 		start_date,
-    end_date,
-    search
+		end_date,
+		search,
 	} = req.query
 
 	if (!property_type) {
-		return res.status(422).json({ message: 'Property type is required.' })
+		return res.status(422).json({ error: 'Property type is required.' })
 	}
-  let dateDifference;
-  let query = [];
+	let dateDifference
+	let query = []
 
 	if (start_date && end_date) {
 		const startDate = moment(start_date, 'YYYY-MM-DD')
 		const endDate = moment(end_date, 'YYYY-MM-DD')
 		if (startDate.isAfter(endDate)) {
 			return res.status(400).json({
-				message: 'start_date cannot be greater than end_date',
+				error: 'start_date cannot be greater than end_date',
 			})
 		}
 
 		if (!startDate.isValid() || !endDate.isValid()) {
-			return res.status(200).json({ message: 'Invalid date format. Use YYYY-MM-DD.' })
+			return res.status(400).json({ error: 'Invalid date format. Use YYYY-MM-DD.' })
 		}
 
 		const today = moment().startOf('day')
 		if (startDate.isBefore(today) || endDate.isBefore(today)) {
-			return res.status(200).json({ message: 'Dates cannot be in the past.' })
+			return res.status(400).json({ error: 'Dates cannot be in the past.' })
 		}
 
 		dateDifference = endDate.diff(startDate, 'days')
 
-    query.push(
-		databaseHelper.GenerateMatchQuery({
-			['availability.availability_365']: { $gte: dateDifference },
-			min_nights: { $lte: dateDifference },
-			max_nights: { $gte: dateDifference },
-		})
-	)
+		query.push(
+			databaseHelper.GenerateMatchQuery({
+				['availability.availability_365']: { $gte: dateDifference },
+				min_nights: { $lte: dateDifference },
+				max_nights: { $gte: dateDifference },
+			})
+		)
 	}
 	if (accommodates) {
-    query.push( databaseHelper.GenerateMatchQuery( 
-      {	
-			accommodates: { $lte: parseInt(accommodates) },
-		}
-    ))
+		query.push(
+			databaseHelper.GenerateMatchQuery({
+				accommodates: { $lte: parseInt(accommodates) },
+			})
+		)
 	}
 
-	// console.log(matchQuery)
-	 query = [
+	query = [
 		databaseHelper.GenerateAddFieldQuery({
 			max_nights: { $toInt: '$maximum_nights' },
 			min_nights: { $toInt: '$minimum_nights' },
 		}),
-		databaseHelper.GenerateMatchQuery({property_type}),
-  ]
-  if ( search ) {
-    query.push(databaseHelper.GenerateSearchQuery(['name'], search))
-  }
-  
-  const options = {
-    startIndex: +startIndex,
-    itemsPerPage: +itemsPerPage,
-    projection: {
-      listing_url: 1,
-      name: 1,
-      bedrooms: 1,
-      host: 1,
-      beds: 1,
-      accommodates: 1,
-      availability_365: 1,
-      // minimum_nights: 1,
-      // maximum_nights: 1,
-      property_type: 1,
-      min_nights: 1,
-      max_nights: 1
-    },
-    query,
-    sortObj:{name:1}
+		databaseHelper.GenerateMatchQuery({ property_type }),
+	]
+	if (search) {
+		query.push(databaseHelper.GenerateSearchQuery(['name'], search))
+	}
+
+	const options = {
+		startIndex: +startIndex,
+		itemsPerPage: +itemsPerPage,
+		projection: {
+			listing_url: 1,
+			name: 1,
+			bedrooms: 1,
+			host: 1,
+			beds: 1,
+			accommodates: 1,
+			availability_365: 1,
+			// minimum_nights: 1,
+			// maximum_nights: 1,
+			property_type: 1,
+			min_nights: 1,
+			max_nights: 1,
+		},
+		query,
+		sortObj: { name: 1 },
 	}
 	const response = await databaseHelper.GenerateQueryWithPagination(options, Airbnb)
-	return res.json({ ...response,  })
+	return res.status(200).json({ ...response })
+  } catch (error) {
+    console.log( error )
+    res.status(500).json({error: error.message})
+  }
 }
